c88a6524888b99bb34af05f063baa9f22fef8d46
Mehdi Jussm <mehdi.jusseaume@gmail.com>
2022-10-21 13:48:06 +0200

    Populate the board

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('Hello World !');
+
+const board = document.getElementById("sketch");
+
+for (i = 0; i < 256; i++) {
+    const div = document.createElement('div');
+    div.innerText="Woop";
+    board.appendChild(div);
+}
+
+console.log(board.childElementCount);
