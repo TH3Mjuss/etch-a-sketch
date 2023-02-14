6678fd6b99ad7416d0f094e38fafdbf6a84208f9
Mehdi Jussm <mehdi.jusseaume@proton.me>
2023-02-14 16:57:22 +0100

    Add default size when user doesn't use prompt, Clear the DOM when the size is changed

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 const board = document.getElementById("sketch");
 
-for (i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.innerText="Woop";
-    board.appendChild(div);
-}
+console.log(board);
+
+function gridSize(defaultSize) {
+    
+    if (board.childElementCount != 0) {
+        console.log('KO');
+        board.innerHTML = '';
+    }
+
+    let size = Number(prompt("Choose your desired grid size (max 100):", "16"));
+
+    if (size < 4 || size > 100) {
+        alert("Incorrect size. Size expected between 4 and 100. Default size set to 16.")
+        size = defaultSize;
+    }
+
+
+    let totalSize = size * size;
+
+    console.log("grid size will be: " + totalSize);
+
+    for (i = 0; i < totalSize; i++) {
+        const div = document.createElement('div');
+        div.innerText="i";
+        board.appendChild(div);
+    }
+
+    console.log("i = " + i)
+
+//    return totalSize;
+};
 
 console.log("Grid size: " + board.childElementCount);
-
-function gridSize() {
-    let size= Number(prompt("Choose your desired grid size (max 100):", "16"));
-
-    console.log(typeof(size));
-
-    console.log("grid size will be: " + size);
-};
